7517633ec2ec1c2a082d43babd24e08dc7382cf4
dongkaishu <dongkaishu@qq.com>
2020-06-19 14:47:15 +0800

    021

---
+++ src/Solution_021.java
@@ -0,0 +1,67 @@
+/**
+ * 合并两个有序链表
+ * <p>
+ * 将两个升序链表合并为一个新的 升序 链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。
+ * <p>
+ * 示例：
+ * <p>
+ * 输入：1->2->4, 1->3->4
+ * 输出：1->1->2->3->4->4
+ */
+
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ * int val;
+ * ListNode next;
+ * ListNode() {}
+ * ListNode(int val) { this.val = val; }
+ * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+public class Solution_021 {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if (l1 == null) return l2;
+        if (l2 == null) return l1;
+        ListNode pre = new ListNode();
+        ListNode result = pre;
+        while (l1 != null && l2 != null) {
+            if (l1.val <= l2.val) {
+                result.next = l1;
+                l1 = l1.next;
+            } else {
+                result.next = l2;
+                l2 = l2.next;
+            }
+            result = result.next;
+        }
+        result.next = l1 == null ? l2 : l1;
+        return pre.next;
+    }
+
+    public static void main(String[] args) {
+        long start = System.nanoTime();
+
+
+        System.out.println(System.nanoTime() - start);
+
+    }
+
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+}
